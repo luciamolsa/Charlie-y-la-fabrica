ffa551a78a32e2180c768f4927353da706321ad5
luciamols <luciamolsa@gmail.com>
2024-11-09 18:12:26 -0300

    remove duplicated code

--- src/Base.cpp
+++ src/Base.cpp
@@ -11,7 +11,6 @@ Base::Base(){
     _base.setTexture(_texture);
     _base.setPosition(360,550);
     _velocity = 4;
-    _base.setOrigin(_base.getGlobalBounds().width/2,0);// comentario de prueba rama refactor
 }
 
 void Base::update(){
